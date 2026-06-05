5ccc74c9f007c97fd2c38866c2b76bb262ea3c72
khilonjiya3 <pankajdoleyapdcl@gmail.com>
2026-06-05 14:45:53 +0530

    Create theme_mode_notifier.dart

---
+++ attendance_payroll_app/lib/core/theme/theme_mode_notifier.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final themeModeProvider = StateNotifierProvider<ThemeModeNotifier, ThemeMode>((ref) {
+  return ThemeModeNotifier();
+});
+
+class ThemeModeNotifier extends StateNotifier<ThemeMode> {
+  static const _key = 'theme_mode';
+
+  ThemeModeNotifier() : super(ThemeMode.system) {
+    _load();
+  }
+
+  Future<void> _load() async {
+    final prefs = await SharedPreferences.getInstance();
+    final val = prefs.getString(_key);
+    if (val == 'dark') state = ThemeMode.dark;
+    else if (val == 'light') state = ThemeMode.light;
+    else state = ThemeMode.system;
+  }
+
+  Future<void> setTheme(ThemeMode mode) async {
+    state = mode;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_key, mode.name);
+  }
+}
