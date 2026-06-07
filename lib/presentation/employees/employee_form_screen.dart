2f410749edeb63bce87c57f8272b3c3cb9db5a5f
khilonjiya3 <pankajdoleyapdcl@gmail.com>
2026-06-07 13:54:05 +0530

    Update employee_form_screen.dart

--- lib/presentation/employees/employee_form_screen.dart
+++ lib/presentation/employees/employee_form_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
+import '../shared/widgets.dart' as w;
 
 import '../../core/theme/app_theme.dart';
 import '../../core/utils/app_utils.dart';
@@ -514,5 +515,3 @@ class _DetailRow {
   }
 }
 
-// Re-export widgets for use in other files
-import '../shared/widgets.dart' as w;
